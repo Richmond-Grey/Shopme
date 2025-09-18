36b0359f37a6fe267ecac755040835a01d497d62
Richmond <grayblack1819@gmail.com>
2025-09-18 04:08:27 +0100

    Generate HTML with Javascript

---
+++ scripts/amazon.js
@@ -0,0 +1,85 @@
+// Storing the details of the products in an array
+const products = [{
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+        rate: 4.5,
+        count: 87
+    },
+    priceCents: 1090
+}, {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+        rate: 4,
+        count: 127
+    },
+    priceCents: 2095
+}, {
+    image: "images/products/liquid-laundry-detergent-plain.jpg",
+    name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
+    rating: {
+        rate: 4.5,
+        count: 305
+    },
+    priceCents: 2899
+}]
+
+let productsHTML = '';
+// Creating HTML for each product
+products.forEach((product) => {
+    productsHTML += `<div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.rate * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>
+`;
+
+})
+
+console.log(productsHTML);
+// Adding the HTML to the page
+document.querySelector('.js-products-grid').innerHTML = productsHTML
