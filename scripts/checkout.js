40f4383f4a95b2d65bc3addbe001de340d36154d
Richmond <grayblack1819@gmail.com>
2025-09-24 16:43:28 +0100

    Regenarting the html when things make a change, so there is no reason to refresh the page or use dom a lot.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -139,6 +139,7 @@ function renderOrderSummary(){
     element.addEventListener(('click'), () => {
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummary();
     });
   });
 }
