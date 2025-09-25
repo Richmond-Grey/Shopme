4b03285100d9891d5a8f823c9e4daaa629cbdd97
Richmond <grayblack1819@gmail.com>
2025-09-25 05:43:25 +0100

    Save data and generate html for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -16,3 +16,16 @@ export const deliveryOptions = [
     }
 
 ];
+
+export function getDeliveryOption(deliveryOptionId){
+     let deliveryOption;
+
+      //Using the delivery option ID we can search for the delivery option in the deliveryOptions.js
+      deliveryOptions.forEach((option) => {
+        if(option.id === deliveryOptionId){
+        deliveryOption = option;
+          }
+        });
+
+      return deliveryOption || deliveryOptions[0];
+}
