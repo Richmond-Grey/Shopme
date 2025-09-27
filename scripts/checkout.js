2a7232b4fe7b58f4e878217572567a6a48c44253
Richmond <grayblack1819@gmail.com>
2025-09-27 05:23:02 +0100

    Convert frompromises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,11 +4,20 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/backend-practice.js'
 import { loadProductsFetch, loadProducts } from "../data/products.js"; 
 
+async function loadPage(){
+
+    await loadProductsFetch();
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+loadPage();
+
+/*
 loadProductsFetch().then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 })
-
+*/
 /*
 loadProducts(() => {
     renderOrderSummary();
